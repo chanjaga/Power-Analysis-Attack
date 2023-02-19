492fee78c713b12c1250c159489bb375e2da48a5
chanjaga <124494895+chanjaga@users.noreply.github.com>
2023-02-19 19:47:46 +0900

    Add files via upload

--- data_converter_tb.v
+++ data_converter_tb.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ns
+`timescale 1ns/1ns
 
 module data_converter_tb;
 
@@ -6,26 +6,32 @@ reg [7:0] data_in;
 wire [7:0] data_out_1;
 wire [7:0] data_out_2;
 wire [3:0] hamming_sum;
+reg clk;
 
 data_converter dut (
     .data_in(data_in),
+    .clk(clk),
     .data_out_1(data_out_1),
     .data_out_2(data_out_2),
     .hamming_sum(hamming_sum)
 );
 
+integer i;
+
 initial begin
     $dumpfile("data_converter_tb.vcd");
     $dumpvars(0, data_converter_tb);
-
-    for (int i = 0; i < 256; i++) begin
+    clk = 0;
+    data_in = 0;
+    #5;
+    for (i = 0; i < 256; i = i + 1) begin
         data_in = i;
-        #10;
-        $display("data_in = %d, data_out_1 = %b, data_out_2 = %b, hamming_sum = %d",
-                 data_in, data_out_1, data_out_2, hamming_sum);
+        #5 clk = ~clk;
     end
 
     $finish;
 end
 
 endmodule
+
+

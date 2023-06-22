a153a59da49f779c2bf2865c481461bbe9cc1ab9
chanjaga <124494895+chanjaga@users.noreply.github.com>
2023-06-22 19:43:14 +0900

    Update CMA_add_simulation_rev.py

--- CMA_add_simulation_rev.py
+++ CMA_add_simulation_rev.py
@@ -3,9 +3,16 @@
 #任意ビット
 
 import itertools
-from tqdm import tqdm
+from matplotlib import pyplot as plt
+#from tqdm import tqdm
 
+# グラフ描画に用いる変数
 count = 0
+dec_count_list = []
+dec_count = 0
+x = []
+y = []
+
 #ここでビット幅を指定
 bit_number = 10
 # 2つの8要素の配列を作成し、要素に1または0を代入
@@ -13,8 +20,11 @@ a = [0]*bit_number
 b = [0]*bit_number
 
 # 任意ビットの加算を全パターン行う
-for a in tqdm(itertools.product([0, 1], repeat = bit_number)):
+#for a in tqdm(itertools.product([0, 1], repeat = bit_number)):
+for a in itertools.product([0, 1], repeat = bit_number):
   for b in itertools.product([0, 1], repeat = bit_number):
+
+    count += 1
 
     # 初期化
     carry = 0
@@ -61,8 +71,8 @@ for a in tqdm(itertools.product([0, 1], repeat = bit_number)):
     result_a = [0]*bit_number
     carry_sub = 0
     for j in range(bit_number):
-        bit1_sub = result[bit_number - 1 -j]   # array1のビット値
-        bit2_sub = minas_b[bit_number -1 -j]    # array2のビット値
+        bit1_sub = result[bit_number - 1 -j]   
+        bit2_sub = minas_b[bit_number -1 -j]    
         
         # 加算処理
         # 桁上げ出力は常に0なので加算処理に含めない
@@ -72,7 +82,7 @@ for a in tqdm(itertools.product([0, 1], repeat = bit_number)):
     # 結果を表示
     #a = list(a)
     if list(a) == result_a:
-      count += 1
+      dec_count += 1
       #print("復号可能")
       #print(f"a:           {list(a)}")
       #print(f"b:           {list(b)}")
@@ -82,5 +92,21 @@ for a in tqdm(itertools.product([0, 1], repeat = bit_number)):
       #print(f"Result:      {result}\n")
       #print(f"Result - b = {result_a}\n")
 
+      # 復号できた入力パターンとcountを記憶
+      dec_count_list.append(count)
+      # aとbをそれぞれjoinしてint型に変換し，10進数に変換，リストに格納
+      x.append(int("".join(map(str, a)), 2))
+      y.append(int("".join(map(str, b)), 2))
 
-print(f"復号可能回数:      {count}")
+print(f"復号可能回数:      {dec_count}")
+
+# グラフ描画
+plt.xlabel("a(decimal)")
+plt.ylabel("b(decimal)")
+
+# タイトルを表示
+plt.title("Decryptable patterns")
+
+# 散布図を描画
+plt.scatter(x, y, s=5, c='blue', marker='o', label='decodable')
+
